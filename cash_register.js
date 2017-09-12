6eee94d28f1501ec6b40bff28267bdb064e45f98
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-11 17:10:12 -1000

    Cleaning up code

--- cash_register.js
+++ cash_register.js
@@ -4,9 +4,21 @@ var currentNumbers = []; // every time we initiate an operation (add or subtract
 var currentOperation = null;
 var keys = document.querySelectorAll(".keys");
 var calculatorFramework = document.querySelector(".calculatorFramework");
+var addButton = document.getElementById("addButton");
+var subtractButton = document.getElementById("subtractButton");
+var multiplyButton = document.getElementById("multiplyButton");
+var divideButton = document.getElementById("divideButton");
+var equalButton = document.getElementById("equalButton");
+
+
+
  // resets current internal state
 function clearCurrentNumbers(){
     currentNumbers = [];
+}
+
+var DOMstrings = {
+      numberHolder: ".numberHolder",
 };
 
 
@@ -14,83 +26,86 @@ function clearCurrentNumbers(){
 
 // hitting all the KEY ID's, querySelectorALL makes it a DOM collection
 function addKeyClickHandlers(key){
-    key.addEventListener("click", function(event){
-      var stringValue = event.currentTarget.dataset.number;
-      currentNumbers.push(stringValue);
-      // Shows clicked number in UI
-      document.querySelector(DOMstrings.numberHolder).value = currentNumbers.join('');
+  key.addEventListener("click", function(event){
+  var stringValue = event.currentTarget.dataset.number;
+  currentNumbers.push(stringValue);
+  // Shows clicked number in UI
+  document.querySelector(DOMstrings.numberHolder).value = currentNumbers.join('');
 
-    });
-};
+  });
+}
 
-    keys.forEach(addKeyClickHandlers);
+keys.forEach(addKeyClickHandlers);
 
+
+document.addEventListener("keypress", function(event) {
+// if ENTER is pressed, return something (event.which is for older browswers)
+if(event.keyCode === 49 || event.which === 49){
+  console.log("1");
+  }
+});
 
 
 
 
 /////////////////////////// CALCULATOR OPERATIONS
-
-var addButton = document.getElementById("addButton");
-    addButton.addEventListener("click", function(event){
+addButton.addEventListener("click", function(event){
         //Changes currentOperation from null to "add" in equalsButton loop
-        currentOperation = "add";
+  currentOperation = "add";
         // save the current set of numbers inputted by user
-        var currentNumberToLoad = parseFloat(currentNumbers.join('')); // ["1", "2", "5"] --> "125"
+  var currentNumberToLoad = parseFloat(currentNumbers.join('')); // ["1", "2", "5"] --> "125"
         // loads current number into calculator
-        calculator.load(currentNumberToLoad);
-        clearCurrentNumbers();
-        document.querySelector(DOMstrings.numberHolder).value = "+";
+  calculator.load(currentNumberToLoad);
+  clearCurrentNumbers();
+  document.querySelector(DOMstrings.numberHolder).value = "+";
 });
 
-var subtractButton = document.getElementById("subtractButton");
-    subtractButton.addEventListener("click", function(event){
-        currentOperation = "subtract";
-        var currentNumberToLoad = parseFloat(currentNumbers.join('')); // 125
-        calculator.load(currentNumberToLoad);
-        clearCurrentNumbers();
-        document.querySelector(DOMstrings.numberHolder).value = "-";
+subtractButton.addEventListener("click", function(event){
+  currentOperation = "subtract";
+  var currentNumberToLoad = parseFloat(currentNumbers.join('')); // 125
+  calculator.load(currentNumberToLoad);
+  clearCurrentNumbers();
+  document.querySelector(DOMstrings.numberHolder).value = "-";
 });
 
-var multiplyButton = document.getElementById("multiplyButton");
-    multiplyButton.addEventListener("click", function(event){
-        currentOperation = "multiply";
-        var currentNumberToLoad = parseFloat(currentNumbers.join(''));
-        calculator.load(currentNumberToLoad);
-        clearCurrentNumbers();
-        document.querySelector(DOMstrings.numberHolder).value = "x";
+multiplyButton.addEventListener("click", function(event){
+  currentOperation = "multiply";
+  var currentNumberToLoad = parseFloat(currentNumbers.join(''));
+  calculator.load(currentNumberToLoad);
+  clearCurrentNumbers();
+  document.querySelector(DOMstrings.numberHolder).value = "x";
 
-});
-
-var divideButton = document.getElementById("divideButton");
-    divideButton.addEventListener("click", function(event){
-        currentOperation = "divide";
-        var currentNumberToLoad = parseFloat(currentNumbers.join(''));
-        calculator.load(currentNumberToLoad);
-        clearCurrentNumbers();
-        document.querySelector(DOMstrings.numberHolder).value = "÷";
 });
 
 
-var equalButton = document.getElementById("equalButton");
-    equalButton.addEventListener("click", function(event){
-        var currentNumberToLoad = parseFloat(currentNumbers.join("")); //456
+divideButton.addEventListener("click", function(event){
+  currentOperation = "divide";
+  var currentNumberToLoad = parseFloat(currentNumbers.join(''));
+  calculator.load(currentNumberToLoad);
+  clearCurrentNumbers();
+  document.querySelector(DOMstrings.numberHolder).value = "÷";
+});
 
 
-    //when user hits EQUAL, check what current operation is
-    if(currentOperation === "add"){
+
+equalButton.addEventListener("click", function(event){
+  var currentNumberToLoad = parseFloat(currentNumbers.join("")); //456
+
+
+  //when user hits EQUAL, check what current operation is
+  if(currentOperation === "add"){
         calculator.add(currentNumberToLoad);
     }
 
-    if(currentOperation === "subtract"){
+  if(currentOperation === "subtract"){
         calculator.subtract(currentNumberToLoad);
     }
 
-    if(currentOperation === "multiply"){
+  if(currentOperation === "multiply"){
         calculator.multiply(currentNumberToLoad);
     }
 
-     if(currentOperation === "divide"){
+  if(currentOperation === "divide"){
         calculator.divide(currentNumberToLoad);
     }
 
@@ -104,12 +119,12 @@ var result = calculator.getTotal();
 
 // CLEARS NUMBER DISPLAY
 var clear = document.getElementById("clearButton");
-    clear.addEventListener("click", (function clearNumbers(){
+    clear.addEventListener("click", function clearNumbers(){
           document.querySelector(DOMstrings.numberHolder).value = null;
           clearCurrentNumbers();
           calculator.clearMemory();
 
-}));
+});
 
 
 // DEPOSIT CASH
@@ -125,36 +140,27 @@ var depoCash = document.getElementById("depoCash");
 }));
 
 
-
-
-
-
-
 // WITHDRAW MONEY
 var withdrawCash = document.getElementById("withCash");
-    withdrawCash.addEventListener("click", (function withdraw(){
-          var moneyWithdraw = parseFloat(document.querySelector(DOMstrings.numberHolder).value);
-          var currentBalance = parseFloat(calculator.recallMemory());
-          calculator.saveMemory(currentBalance - moneyWithdraw);
-          document.querySelector(DOMstrings.numberHolder).value = "Money Withdrawn";
+withdrawCash.addEventListener("click", function withdraw(){
+  var moneyWithdraw = parseFloat(document.querySelector(DOMstrings.numberHolder).value);
+  var currentBalance = parseFloat(calculator.recallMemory());
+  calculator.saveMemory(currentBalance - moneyWithdraw);
+  document.querySelector(DOMstrings.numberHolder).value = "Money Withdrawn";
 
-}));
+});
 
 
 
 
 // GETS BALANCE
 var balance = document.getElementById("getBalance");
-    balance.addEventListener("click", (function balance(){
+    balance.addEventListener("click", function balance(){
           var returnBalance = parseFloat(calculator.recallMemory());
           document.querySelector(DOMstrings.numberHolder).value = returnBalance;
-}));
+});
 
 
 
-
-var DOMstrings = {
-      numberHolder: ".numberHolder",
-  };
 
 
